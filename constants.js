5e17e2d12ad2912b73755007d3632ebf30c9e446
pljjy <jotaro5jostar@gmail.com>
2023-09-08 21:05:00 -0400

    somewhat a working calculator - added all the needed css - added all the needed html - added some js for calculator to work
    BUT a LOT of bugs and doesn't support negative and negative numbers, I
    should fix it soon

--- constants.js
+++ constants.js
@@ -1,6 +1,39 @@
-const arrayToInt = function(array){
-    return parseInt(array.reduce((prev, current) => prev + '' + current));
+const arrayToInt = function (array) {
+  if(array == [] || array.length == 0) return "";
+  return parseInt(array.reduce((prev, current) => prev + "" + current));
+};
+
+const intToArray = function (num){
+  return num.toString().replace(/\D/g, '0').split('').map((element)=>parseInt(element));
 }
 
 
-export{};
+const btnNums = [document.querySelector(".zero"), document.querySelector(".one"), document.querySelector(".two"),
+document.querySelector(".three"), document.querySelector(".four"), document.querySelector(".five"), document.querySelector(".six"),
+document.querySelector(".seven"), document.querySelector(".eight"), document.querySelector(".nine")];
+
+const btnAdd = document.querySelector(".add");
+const btnSub = document.querySelector(".sub");
+const btnDivide = document.querySelector(".divide");
+const btnMultiply = document.querySelector(".multiply");
+const btnDelete = document.querySelector(".delete");
+const btnClear = document.querySelector(".clear");
+const btnEquals = document.querySelector(".equals");
+const currentValue = document.querySelector("#current-value");
+const lastValue = document.querySelector("#last-value");
+
+
+export {
+  intToArray,
+  arrayToInt,
+  btnNums,
+  btnAdd,
+  btnSub,
+  btnDivide,
+  btnMultiply,
+  btnDelete,
+  btnClear,
+  btnEquals,
+  currentValue,
+  lastValue,
+};
--- index.js
+++ index.js
@@ -1,1 +1,115 @@
-import * as calc from "./constants";
+import {
+  intToArray,
+  arrayToInt,
+  btnNums,
+  btnAdd,
+  btnSub,
+  btnDivide,
+  btnMultiply,
+  btnDelete,
+  btnClear,
+  btnEquals,
+  currentValue,
+  lastValue,
+} from "./constants.js";
+
+// btnNums[0] = btn with 0 as text 🫠
+var currentArray = [];
+var lastArray = [];
+var operation;
+
+// FUNCTIONS
+function refreshScreen() {
+  currentValue.textContent = arrayToInt(currentArray);
+  lastValue.textContent = arrayToInt(lastArray);
+}
+
+function concatToCurrentArray(value) {
+  if (currentArray.length >= 13 || value === null) return;
+  currentArray.push(value);
+  currentValue.textContent = arrayToInt(currentArray);
+}
+
+function clearValues() {
+  currentArray = [];
+  lastArray = [];
+  operation = "";
+  refreshScreen();
+}
+
+function deleteLastSymbol() {
+  if (currentArray.length <= 1) {
+    currentArray = [];
+    currentValue.textContent = "";
+  } else {
+    currentArray.pop();
+    currentValue.textContent = arrayToInt(currentArray);
+  }
+}
+
+function equals() {
+  let value1 = arrayToInt(lastArray);
+  let value2 = arrayToInt(currentArray);
+  let result;
+
+  switch (operation) {
+    case "add":
+      result = value1 + value2;
+      break;
+    case "sub":
+      result = value1 - value2;
+      break;
+    case "multiply":
+      result = value1 * value2;
+      break;
+    case "divide":
+      result = value1 / value2;
+      break;
+    default: // if this happens, check assigned value to [operation] in operation functions
+      lastArray = [];
+      refreshScreen();
+      currentValue.textContent = "3RR0R!";
+      return;
+  }
+  currentArray = intToArray(result);
+  lastArray = intToArray(value2);
+  refreshScreen();
+}
+
+function add() {
+  performOperation("add");
+}
+
+function sub() {
+  performOperation("sub");
+}
+
+function multiply() {
+  performOperation("multiply");
+}
+
+function divide() {
+  performOperation("divide");
+}
+
+function performOperation(str) {
+  operation = str;
+  lastArray = currentArray;
+  currentArray = [];
+  refreshScreen();
+}
+
+// EVENT LISTENERS
+btnNums.forEach((element) => {
+  element.addEventListener("click", () =>
+    concatToCurrentArray(element.textContent)
+  );
+});
+btnClear.addEventListener("click", clearValues);
+btnDelete.addEventListener("click", deleteLastSymbol);
+btnEquals.addEventListener("click", equals);
+
+btnAdd.addEventListener("click", add);
+btnSub.addEventListener("click", sub);
+btnMultiply.addEventListener("click", multiply);
+btnDivide.addEventListener("click", divide);
